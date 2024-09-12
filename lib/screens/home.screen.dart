760620324113273b08528b22c8511e48364b9f23
Paurakh Saud <paurakhsaud2022@gmail.com>
2024-09-12 13:16:03 +0545

    weather app competed

--- lib/screens/home.screen.dart
+++ lib/screens/home.screen.dart
@@ -90,9 +90,9 @@ class HomePage extends StatelessWidget {
                         const SizedBox(
                           height: 5,
                         ),
-                        const Text(
-                          "Good Morning",
-                          style: TextStyle(
+                        Text(
+                          getCurrentGreeting(),
+                          style: const TextStyle(
                             fontSize: 25,
                             fontWeight: FontWeight.bold,
                             color: Colors.white,
@@ -341,5 +341,22 @@ class HomePage extends StatelessWidget {
     }
 
     return imagePath;
+  }
+
+  String getCurrentGreeting() {
+    // Get the current time
+    final now = DateTime.now();
+    final hour = now.hour;
+
+    // Determine the greeting based on the hour of the day
+    if (hour >= 5 && hour < 12) {
+      return 'Good Morning';
+    } else if (hour >= 12 && hour < 17) {
+      return 'Good Afternoon';
+    } else if (hour >= 17 && hour < 21) {
+      return 'Good Evening';
+    } else {
+      return 'Good Night';
+    }
   }
 }
